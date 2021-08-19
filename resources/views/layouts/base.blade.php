a73fc5e5297448b276e289d1073edca5ace942b6
venemexcoin <venemexcoin@gmail.com>
2021-08-19 18:55:24 -0500

    commit actualisacion para githib sin secure_

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,151 +1,166 @@
 <!DOCTYPE HTML>
 <!--
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ Landed by HTML5 UP
+ html5up.net | @ajlkn
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>@stack('title_base')</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="shortcut icon" type="image/x-icon" href="{{secure_asset('assets/images/developer-1.ico')}}">
-		<link rel="stylesheet" href="{{secure_asset('assets/css/main.css')}}" />
-		<noscript><link rel="stylesheet" href="{{secure_asset('assets/css/noscript.css')}}" /></noscript>
-		@stack('styles')
-		@livewireStyles
 
-	</head>
-	<body class="is-preload landing">
-		<div id="page-wrapper">
+<head>
+    <title>@stack('title_base')</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/developer-1.ico') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+    <noscript>
+        <link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" />
+    </noscript>
+    @stack('styles')
+    @livewireStyles
 
-			<!-- Header -->
-				<header id="header">
-					<h1 id="logo"><a href="https://herokuproy1.herokuapp.com/">ChamocellDeveloper</a></h1>
-					<nav id="nav">
-						<ul>
-							<li><a href="https://herokuproy1.herokuapp.com/">Home</a></li>
-							 <li class="menu_admin">
-								<a href="#">Layouts</a>
-								<ul>
-									<li><a href="#veremos">Que veremos aquí</a></li>
-									<li><a href="{{route('general.uno')}}">paginas Completas</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Paginas Completas</a></li>
-											<li><a href="#">Option 2</a></li>
-											<li><a href="#">Option 3</a></li>
-											<li><a href="#">Option 4</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li> 
-							@if(Route::has('login'))
-								@auth
-									@if(Auth::user()->utype === 'ADM')
-									<li class="menu_admin">
-										<a href="#">Menu Administrador</a>
-										<ul>
-											<li><a href="{{route('admin.completa')}}">Paginas completas</a></li>
-											<li><a href="right-sidebar.html">Right Sidebar</a></li>
-											<li><a href="no-sidebar.html">No Sidebar</a></li>
-											<li>
-												<a href="#">Submenu</a>
-												<ul>
-													<li><a href="#">Option 1</a></li>
-													<li><a href="#">Option 2</a></li>
-													<li><a href="#">Option 3</a></li>
-													<li><a href="#">Option 4</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a title="My Account" href="#">My Account {{Auth::user()->name}}</a></li>
-											<li><a title="Dashboard" href="{{route('admin.dashboard')}}">Dashboard</a></li>
-											<li><a href="{{'logout'}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-											<form id="logout-form" method="POST" action="{{route('logout')}}">
-												@csrf
-												
-											</form>
-										</ul>
-									</li> 
-									   
-									@else
-									<li class="menu_admin">
-										<a href="#">Menu Usuario</a>
-										<ul>
-											<li><a href="left-sidebar.html">Left Sidebar</a></li>
-											<li><a href="right-sidebar.html">Right Sidebar</a></li>
-											<li><a href="no-sidebar.html">No Sidebar</a></li>
-											<li>
-												<a href="#">Submenu</a>
-												<ul>
-													<li><a href="#">Option 5</a></li>
-													<li><a href="#">Option 6</a></li>
-													<li><a href="#">Option 7</a></li>
-													<li><a href="#">Option 8</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
+</head>
 
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a title="My Account" href="#">My Account {{Auth::user()->name}}</a></li>
-											<li><a title="Dashboard" href="{{route('user.dashboard')}}">Dashboard</a></li>
-											<li><a href="{{'logout'}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-											<form id="logout-form" method="POST" action="{{route('logout')}}">
-												@csrf
-											</form>
-										</ul>
-									</li>  	 
-									@endif
-								@else  
-									<li><a href="{{route('login')}}" class="button primary">Login</a></li>
-									{{-- <li><a href="{{route('register')}}" class="button primary">Register</a></li> --}}
-									
-								@endif
-							@endif
-						</ul>
-					</nav>
-				</header>
+<body class="is-preload landing">
+    <div id="page-wrapper">
 
-			{{$slot}}
+        <!-- Header -->
+        <header id="header">
+            <h1 id="logo"><a href="https://herokuproy1.herokuapp.com/">ChamocellDeveloper</a></h1>
+            <nav id="nav">
+                <ul>
+                    <li><a href="https://herokuproy1.herokuapp.com/">Home</a></li>
+                    <li class="menu_admin">
+                        <a href="#">Layouts</a>
+                        <ul>
+                            <li><a href="#veremos">Que veremos aquí</a></li>
+                            <li><a href="{{ route('general.uno') }}">paginas Completas</a></li>
+                            <li><a href="no-sidebar.html">No Sidebar</a></li>
+                            <li>
+                                <a href="#">Submenu</a>
+                                <ul>
+                                    <li><a href="#">Paginas Completas</a></li>
+                                    <li><a href="#">Option 2</a></li>
+                                    <li><a href="#">Option 3</a></li>
+                                    <li><a href="#">Option 4</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    @if (Route::has('login'))
+                        @auth
+                            @if (Auth::user()->utype === 'ADM')
+                                <li class="menu_admin">
+                                    <a href="#">Menu Administrador</a>
+                                    <ul>
+                                        <li><a href="{{ route('admin.completa') }}">Paginas completas</a></li>
+                                        <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                                        <li><a href="no-sidebar.html">No Sidebar</a></li>
+                                        <li>
+                                            <a href="#">Submenu</a>
+                                            <ul>
+                                                <li><a href="#">Option 1</a></li>
+                                                <li><a href="#">Option 2</a></li>
+                                                <li><a href="#">Option 3</a></li>
+                                                <li><a href="#">Option 4</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Submenu</a>
+                                    <ul>
+                                        <li><a title="My Account" href="#">My Account {{ Auth::user()->name }}</a></li>
+                                        <li><a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                                        <li><a href="{{ 'logout' }}"
+                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                        </li>
+                                        <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                            @csrf
 
-			<!-- Footer -->
-				<footer id="footer">
-					<ul class="icons">
-						<li><a href="https://twitter.com/chamocelldevel1" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="https://www.facebook.com/alfonso.berroteran.3" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-						<li><a href="https://www.linkedin.com/in/eduardo-liendo-152243120/" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-						<li><a href="https://www.instagram.com/chamocelldeveloper/?hl=es-la" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="https://github.com/venemexcoin" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-						<li><a href="#formImail" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
-					</ul>
-					<ul class="copyright">
-						<li>&copy; Intitulado. Reservados todos los derechos.</li><li>Design: <a href="https://herokuproy1.herokuapp.com/">Mi Perfil</a></li>
-					</ul>
-				</footer>
+                                        </form>
+                                    </ul>
+                                </li>
 
-		</div>
+                            @else
+                                <li class="menu_admin">
+                                    <a href="#">Menu Usuario</a>
+                                    <ul>
+                                        <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                                        <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                                        <li><a href="no-sidebar.html">No Sidebar</a></li>
+                                        <li>
+                                            <a href="#">Submenu</a>
+                                            <ul>
+                                                <li><a href="#">Option 5</a></li>
+                                                <li><a href="#">Option 6</a></li>
+                                                <li><a href="#">Option 7</a></li>
+                                                <li><a href="#">Option 8</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
 
-		<!-- Scripts -->
-			<script src="{{secure_asset('assets/js/jquery.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/jquery.scrolly.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/jquery.dropotron.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/jquery.scrollex.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/browser.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/breakpoints.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/util.js')}}"></script>
-			<script src="{{secure_asset('assets/js/main.js')}}"></script>
-			<script src="https://kit.fontawesome.com/3226d1ef96.js" crossorigin="anonymous"></script> 	
-			@stack('scripts')
-			@livewireScripts
-	</body>
-</html>
+                                <li>
+                                    <a href="#">Submenu</a>
+                                    <ul>
+                                        <li><a title="My Account" href="#">My Account {{ Auth::user()->name }}</a></li>
+                                        <li><a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a></li>
+                                        <li><a href="{{ 'logout' }}"
+                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                        </li>
+                                        <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                        </form>
+                                    </ul>
+                                </li>
+                            @endif
+                        @else
+                            <li><a href="{{ route('login') }}" class="button primary">Login</a></li>
+                            {{-- <li><a href="{{route('register')}}" class="button primary">Register</a></li> --}}
+
+                        @endif
+                        @endif
+                    </ul>
+                </nav>
+            </header>
+
+            {{ $slot }}
+
+            <!-- Footer -->
+            <footer id="footer">
+                <ul class="icons">
+                    <li><a href="https://twitter.com/chamocelldevel1" class="icon brands alt fa-twitter"><span
+                                class="label">Twitter</span></a></li>
+                    <li><a href="https://www.facebook.com/alfonso.berroteran.3" class="icon brands alt fa-facebook-f"><span
+                                class="label">Facebook</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/eduardo-liendo-152243120/"
+                            class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+                    <li><a href="https://www.instagram.com/chamocelldeveloper/?hl=es-la"
+                            class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="https://github.com/venemexcoin" class="icon brands alt fa-github"><span
+                                class="label">GitHub</span></a></li>
+                    <li><a href="#formImail" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+                </ul>
+                <ul class="copyright">
+                    <li>&copy; Intitulado. Reservados todos los derechos.</li>
+                    <li>Design: <a href="https://herokuproy1.herokuapp.com/">Mi Perfil</a></li>
+                </ul>
+            </footer>
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.scrolly.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.dropotron.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.scrollex.min.js') }}"></script>
+        <script src="{{ asset('assets/js/browser.min.js') }}"></script>
+        <script src="{{ asset('assets/js/breakpoints.min.js') }}"></script>
+        <script src="{{ asset('assets/js/util.js') }}"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>
+        <script src="https://kit.fontawesome.com/3226d1ef96.js" crossorigin="anonymous"></script>
+        @stack('scripts')
+        @livewireScripts
+    </body>
+
+    </html>
--- resources/views/layouts/base1.blade.php
+++ resources/views/layouts/base1.blade.php
@@ -1,137 +1,146 @@
 <!DOCTYPE HTML>
 <!--
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ Landed by HTML5 UP
+ html5up.net | @ajlkn
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>@stack('title_base')</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="shortcut icon" type="image/x-icon" href="{{secure_asset('assets/images/developer-1.ico')}}">
-		<link rel="stylesheet" href="{{secure_asset('assets/css/main.css')}}" />
-		<noscript><link rel="stylesheet" href="{{secure_asset('assets/css/noscript.css')}}" /></noscript>
-		@stack('styles')
-		@livewireStyles
 
-	</head>
-	<body class="is-preload landing">
-		<div id="page-wrapper">
+<head>
+    <title>@stack('title_base')</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/developer-1.ico') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+    <noscript>
+        <link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" />
+    </noscript>
+    @stack('styles')
+    @livewireStyles
 
-			<!-- Header -->
-				<header id="header">
-					<h1 id="logo"><a href="https://herokuproy1.herokuapp.com/">ChamocellDeveloper</a></h1>
-					<nav id="nav">
-						<ul>
-							<li><a href="https://herokuproy1.herokuapp.com/">Home</a></li>
-							 <li class="menu_admin">
-								<a href="#">Layouts</a>
-								<ul>
-									<li><a href="/">Mi espacio</a></li>
-									<li><a href="{{route('general.uno')}}">Right Sidebar</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Paginas Completas</a></li>
-											<li><a href="#">Option 2</a></li>
-											<li><a href="#">Option 3</a></li>
-											<li><a href="#">Option 4</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li> 
-							@if(Route::has('login'))
-								@auth
-									@if(Auth::user()->utype === 'ADM')
-									<li class="menu_admin">
-										<a href="#">Menu Administrador</a>
-										<ul>
-											<li><a href="{{route('admin.completa')}}">Paginas completas</a></li>
-											<li><a href="right-sidebar.html">Right Sidebar</a></li>
-											<li><a href="no-sidebar.html">No Sidebar</a></li>
-											<li>
-												<a href="#">Submenu</a>
-												<ul>
-													<li><a href="#">Option 1</a></li>
-													<li><a href="#">Option 2</a></li>
-													<li><a href="#">Option 3</a></li>
-													<li><a href="#">Option 4</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a title="My Account" href="#">My Account {{Auth::user()->name}}</a></li>
-											<li><a title="Dashboard" href="{{route('admin.dashboard')}}">Dashboard</a></li>
-											<li><a href="{{'logout'}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-											<form id="logout-form" method="POST" action="{{route('logout')}}">
-												@csrf
-												
-											</form>
-										</ul>
-									</li> 
-									   
-									@else
-									<li class="menu_admin">
-										<a href="#">Menu Usuario</a>
-										<ul>
-											<li><a href="left-sidebar.html">Left Sidebar</a></li>
-											<li><a href="right-sidebar.html">Right Sidebar</a></li>
-											<li><a href="no-sidebar.html">No Sidebar</a></li>
-											<li>
-												<a href="#">Submenu</a>
-												<ul>
-													<li><a href="#">Option 5</a></li>
-													<li><a href="#">Option 6</a></li>
-													<li><a href="#">Option 7</a></li>
-													<li><a href="#">Option 8</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
+</head>
 
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a title="My Account" href="#">My Account {{Auth::user()->name}}</a></li>
-											<li><a title="Dashboard" href="{{route('user.dashboard')}}">Dashboard</a></li>
-											<li><a href="{{'logout'}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-											<form id="logout-form" method="POST" action="{{route('logout')}}">
-												@csrf
-											</form>
-										</ul>
-									</li>  	 
-									@endif
-								@else  
-									<li><a href="{{route('login')}}" class="button primary">Login</a></li>
-									{{-- <li><a href="{{route('register')}}" class="button primary">Register</a></li> --}}
-									
-								@endif
-							@endif
-						</ul>
-					</nav>
-				</header>
+<body class="is-preload landing">
+    <div id="page-wrapper">
 
-			{{$slot}}
+        <!-- Header -->
+        <header id="header">
+            <h1 id="logo"><a href="https://herokuproy1.herokuapp.com/">ChamocellDeveloper</a></h1>
+            <nav id="nav">
+                <ul>
+                    <li><a href="https://herokuproy1.herokuapp.com/">Home</a></li>
+                    <li class="menu_admin">
+                        <a href="#">Layouts</a>
+                        <ul>
+                            <li><a href="/">Mi espacio</a></li>
+                            <li><a href="{{ route('general.uno') }}">Right Sidebar</a></li>
+                            <li><a href="no-sidebar.html">No Sidebar</a></li>
+                            <li>
+                                <a href="#">Submenu</a>
+                                <ul>
+                                    <li><a href="#">Paginas Completas</a></li>
+                                    <li><a href="#">Option 2</a></li>
+                                    <li><a href="#">Option 3</a></li>
+                                    <li><a href="#">Option 4</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    @if (Route::has('login'))
+                        @auth
+                            @if (Auth::user()->utype === 'ADM')
+                                <li class="menu_admin">
+                                    <a href="#">Menu Administrador</a>
+                                    <ul>
+                                        <li><a href="{{ route('admin.completa') }}">Paginas completas</a></li>
+                                        <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                                        <li><a href="no-sidebar.html">No Sidebar</a></li>
+                                        <li>
+                                            <a href="#">Submenu</a>
+                                            <ul>
+                                                <li><a href="#">Option 1</a></li>
+                                                <li><a href="#">Option 2</a></li>
+                                                <li><a href="#">Option 3</a></li>
+                                                <li><a href="#">Option 4</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Submenu</a>
+                                    <ul>
+                                        <li><a title="My Account" href="#">My Account {{ Auth::user()->name }}</a></li>
+                                        <li><a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                                        <li><a href="{{ 'logout' }}"
+                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                        </li>
+                                        <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                            @csrf
+
+                                        </form>
+                                    </ul>
+                                </li>
+
+                            @else
+                                <li class="menu_admin">
+                                    <a href="#">Menu Usuario</a>
+                                    <ul>
+                                        <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                                        <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                                        <li><a href="no-sidebar.html">No Sidebar</a></li>
+                                        <li>
+                                            <a href="#">Submenu</a>
+                                            <ul>
+                                                <li><a href="#">Option 5</a></li>
+                                                <li><a href="#">Option 6</a></li>
+                                                <li><a href="#">Option 7</a></li>
+                                                <li><a href="#">Option 8</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li>
+                                    <a href="#">Submenu</a>
+                                    <ul>
+                                        <li><a title="My Account" href="#">My Account {{ Auth::user()->name }}</a></li>
+                                        <li><a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a></li>
+                                        <li><a href="{{ 'logout' }}"
+                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                        </li>
+                                        <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                        </form>
+                                    </ul>
+                                </li>
+                            @endif
+                        @else
+                            <li><a href="{{ route('login') }}" class="button primary">Login</a></li>
+                            {{-- <li><a href="{{route('register')}}" class="button primary">Register</a></li> --}}
+
+                        @endif
+                        @endif
+                    </ul>
+                </nav>
+            </header>
+
+            {{ $slot }}
 
 
-		</div>
+        </div>
 
-		<!-- Scripts -->
-			<script src="{{secure_asset('assets/js/jquery.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/jquery.scrolly.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/jquery.dropotron.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/jquery.scrollex.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/browser.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/breakpoints.min.js')}}"></script>
-			<script src="{{secure_asset('assets/js/util.js')}}"></script>
-			<script src="{{secure_asset('assets/js/main.js')}}"></script>
-			<script src="https://kit.fontawesome.com/3226d1ef96.js" crossorigin="anonymous"></script> 	
-			@stack('scripts')
-			@livewireScripts
-	</body>
-</html>
+        <!-- Scripts -->
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.scrolly.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.dropotron.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.scrollex.min.js') }}"></script>
+        <script src="{{ asset('assets/js/browser.min.js') }}"></script>
+        <script src="{{ asset('assets/js/breakpoints.min.js') }}"></script>
+        <script src="{{ asset('assets/js/util.js') }}"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>
+        <script src="https://kit.fontawesome.com/3226d1ef96.js" crossorigin="anonymous"></script>
+        @stack('scripts')
+        @livewireScripts
+    </body>
+
+    </html>
