5f2bfc0c3d0fbbfd952b8a2d85c7cdca50851250
venemexcoin <venemexcoin@gmail.com>
2021-08-21 01:33:53 -0500

    commit agregamos distintos loyauts a la aplicación

--- resources/views/layouts/daraz.blade.php
+++ resources/views/layouts/daraz.blade.php
@@ -13,17 +13,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Daraz by TEMPLATE STOCK</title>
 
-    <link rel="stylesheet" href="{{secure_asset('assets/darraz/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{secure_asset('assets/darraz/css/font-awesome.min.css') }}" />
-    <link rel="stylesheet" href="{{secure_asset('assets/darraz/css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/darraz/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/darraz/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/darraz/css/animate.css') }}" />
 
-    <link rel="stylesheet" href="{{secure_asset('assets/darraz/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/darraz/css/style.css') }}" />
 
-    <script type="text/javascript" src="{{secure_asset('assets/darraz/js/jquery-1.11.2.min.js') }}"></script>
-    <script type="text/javascript" src="{{secure_asset('assets/darraz/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/darraz/js/jquery-1.11.2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/darraz/js/bootstrap.min.js') }}"></script>
     <script type="text/javascript"
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZXJBVDf7R4JqmSpopVPoduIGWx1IwpBM"></script>
-    <script type="text/javascript" src="{{secure_asset('assets/darraz/js/plugins.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/darraz/js/plugins.js') }}"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -145,17 +145,17 @@
 
                 <script type="text/javascript">
                     var dataHeader = [{
-                                bigImage: "{{secure_asset('assets/darraz/images/slide-1.jpg') }}",
+                                bigImage: "{{ asset('assets/darraz/images/slide-1.jpg') }}",
                                 title: "Minimal & Clean",
                                 author: "Templatestock"
                             },
                             {
-                                bigImage: "{{secure_asset('assets/darraz/images/slide-2.jpg') }}",
+                                bigImage: "{{ asset('assets/darraz/images/slide-2.jpg') }}",
                                 title: "I'm A Creative Theme",
                                 author: "Templatestock"
                             },
                             {
-                                bigImage: "{{secure_asset('assets/darraz/images/slide-3.jpg') }}",
+                                bigImage: "{{ asset('assets/darraz/images/slide-3.jpg') }}",
                                 title: "Achieve Success",
                                 author: "Templatestock"
                             }
@@ -254,49 +254,49 @@
                         <script type="text/javascript">
                             var portfolio = [{
                                     category: "branding",
-                                    image: "{{secure_asset('assets/darraz/images/p-1.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-1.png') }}",
                                     title: "Charming <span>Roxy</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
                                 },
                                 {
                                     category: "graphic",
-                                    image: "{{secure_asset('assets/darraz/images/p-2.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-2.png') }}",
                                     title: "Fresh <span>Bubba</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
                                 },
                                 {
                                     category: "graphic",
-                                    image: "{{secure_asset('assets/darraz/images/p-3.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-3.png') }}",
                                     title: "Wild <span>Romeo</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
                                 },
                                 {
                                     category: "video",
-                                    image: "{{secure_asset('assets/darraz/images/p-4.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-4.png') }}",
                                     title: "Strange <span>Dexter</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
                                 },
                                 {
                                     category: "branding",
-                                    image: "{{secure_asset('assets/darraz/images/p-5.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-5.png') }}",
                                     title: "Free <span>Sarah</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
                                 },
                                 {
                                     category: "printing",
-                                    image: "{{secure_asset('assets/darraz/images/p-6.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-6.png') }}",
                                     title: "Chico <span>Silly</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
                                 },
                                 {
                                     category: "printing",
-                                    image: "{{secure_asset('assets/darraz/images/p-7.png') }}",
+                                    image: "{{ asset('assets/darraz/images/p-7.png') }}",
                                     title: "IG <span>Shop</span>",
                                     link: "#none",
                                     text: "Lorem ipsum Dolor in minim fugiat ut nisi occaecat fugiat. Lorem ipsum Quis tempor Ut enim officia deserunt consectetur."
@@ -330,7 +330,7 @@
                     <div class="col-md-3 col-sm-6 text-center">
                         <div class="wrap animated" data-animate="fadeInDown">
                             <div class="img-team">
-                                <img src="{{secure_asset('assets/darraz/images/team-1.png') }}" alt="" class="img-circle">
+                                <img src="{{ asset('assets/darraz/images/team-1.png') }}" alt="" class="img-circle">
                             </div>
 
                             <h3>John Doe</h3>
@@ -353,7 +353,7 @@
                     <div class="col-md-3 col-sm-6 text-center">
                         <div class="wrap animated" data-animate="fadeInDown">
                             <div class="img-team">
-                                <img src="{{secure_asset('assets/darraz/images/team-2.png') }}" alt="" class="img-circle">
+                                <img src="{{ asset('assets/darraz/images/team-2.png') }}" alt="" class="img-circle">
                             </div>
 
                             <h3>Ellie Morgan</h3>
@@ -376,7 +376,7 @@
                     <div class="col-md-3 col-sm-6 text-center">
                         <div class="wrap animated" data-animate="fadeInDown">
                             <div class="img-team">
-                                <img src="{{secure_asset('assets/darraz/images/team-3.png') }}" alt="" class="img-circle">
+                                <img src="{{ asset('assets/darraz/images/team-3.png') }}" alt="" class="img-circle">
                             </div>
 
                             <h3>Elche</h3>
@@ -399,7 +399,7 @@
                     <div class="col-md-3 col-sm-6 text-center">
                         <div class="wrap animated" data-animate="fadeInDown">
                             <div class="img-team">
-                                <img src="{{secure_asset('assets/darraz/images/team-4.png') }}" alt="" class="img-circle">
+                                <img src="{{ asset('assets/darraz/images/team-4.png') }}" alt="" class="img-circle">
                             </div>
 
                             <h3>JF Thiago</h3>
@@ -541,7 +541,7 @@
     </div><!-- /.container-fluid -->
 
     <!-- SCRIPT -->
-    <script type="text/javascript" src="{{secure_asset('assets/darraz/js/main.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/darraz/js/main.js') }}"></script>
 </body>
 
 </html>
